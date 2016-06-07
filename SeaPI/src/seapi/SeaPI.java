d36cffbbf5d953d73ef774197bc4408849a9079e
rookiebeotch <jorge@alumni.ufl.edu>
2016-06-06 22:59:37 -0400

    test pi4j code

--- SeaPI/src/seapi/SeaPI.java
+++ SeaPI/src/seapi/SeaPI.java
@@ -5,6 +5,13 @@
  */
 package seapi;
 
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.gpio.GpioPinDigitalInput;
+import com.pi4j.io.gpio.GpioPinDigitalOutput;
+import com.pi4j.io.gpio.PinPullResistance;
+import com.pi4j.io.gpio.RaspiPin;
+
 /**
  *
  * @author jorge
@@ -16,6 +23,16 @@ public class SeaPI {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.print("Hey Hello Florida!!");
+        // create gpio controller
+        final GpioController gpio = GpioFactory.getInstance();
+        // provision gpio pin #01 & #03 as an output pins and blink
+        final GpioPinDigitalOutput led1 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01);
+        final GpioPinDigitalOutput led2 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_03);
+        // provision gpio pin #02 as an input pin with its internal pull down resistor enabled
+        final GpioPinDigitalInput myButton = gpio.provisionDigitalInputPin(RaspiPin.GPIO_02, PinPullResistance.PULL_DOWN);
+
+        
     }
     
 }
